838666983fe5ca25cdfb2628c22c55c13a1258a5
Papum <bruno@ntcbrasil.com.br>
2025-08-17 16:01:38 -0300

    Atualizar arquivos do projeto - 2025-08-17T19:01:38.263Z

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
-import Header from './components/Header';
-import Hero from './components/Hero';
-import About from './components/About';
-import Services from './components/Services';
-import Contact from './components/Contact';
-import Footer from './components/Footer';
+import Header from './components/Header.jsx';
+import Hero from './components/Hero.jsx';
+import About from './components/About.jsx';
+import Services from './components/Services.jsx';
+import Contact from './components/Contact.jsx';
+import Footer from './components/Footer.jsx';
 
 export default function App() {
   return (
